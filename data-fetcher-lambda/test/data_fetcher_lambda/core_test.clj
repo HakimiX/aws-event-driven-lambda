579fe99732637a6a0e1f20b424f95212e8c8c1d1
Hakimix <mustafa.hakimi@outlook.dk>
2023-06-15 15:39:51 +0200

    add initial lambda logic that can handle incomming events

---
+++ data-fetcher-lambda/test/data_fetcher_lambda/core_test.clj
@@ -0,0 +1,7 @@
+(ns data-fetcher-lambda.core-test
+  (:require [clojure.test :refer :all]
+            [data-fetcher-lambda.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
